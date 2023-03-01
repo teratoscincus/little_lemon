717bbec2ed41ad26bda6d56e7f30681a4952248b
Max <teratoscincus@hotmail.com>
2023-03-01 15:32:55 +0100

    Install packages and provide settings
    Installed DRF and Djoser and added settings.
    Installed django_filters, and added to list of dependencies.

--- little_lemon/settings.py
+++ little_lemon/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party apps
+    "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -79,9 +84,9 @@ WSGI_APPLICATION = "little_lemon.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        # "NAME": "db_name",  # Read from .my.cnf
-        # "USER": "user",  # Read from .my.cnf
-        # "PASSWORD": "password",  # Read from .my.cnf
+        # "NAME": "db_name",  # NOTE: Read from .my.cnf
+        # "USER": "user",  # NOTE: Read from .my.cnf
+        # "PASSWORD": "password",  # NOTE: Read from .my.cnf
         "HOST": "localhost",
         "PORT": "3306",
         "OPTIONS": {
@@ -137,3 +142,42 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# DRF settings
+
+REST_FRAMEWORK = {
+    # Rendering
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "rest_framework_xml.renderers.XMLRenderer",
+    ],
+    # Authentication
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    # Sorting, Searching, and Pagination
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 2,
+    # Throttling
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "6/minute",
+        "user": "12/minute",
+    },
+}
+
+# Djoser settings
+
+DJOSER = {
+    "USER_ID_FIELD": "username",
+}
